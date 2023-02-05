d75b96846deea694647a02ac4415c881398d55f3
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2023-02-05 17:36:10 +0100

    Add presentation controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { Stage, OrbitControls } from "@react-three/drei";
+import { Stage, PresentationControls } from "@react-three/drei";
 import { useControls } from "leva";
+import { Perf } from "r3f-perf";
 import Pop from "./Pop";
 import Room from "./Room";
 
@@ -8,22 +9,27 @@ export default function App() {
 
   return (
     <>
-      <OrbitControls makeDefault />
+      <Perf position="top-left" />
+
+      <color attach="background" args={[color]} />
 
       <Stage
         intensity={0.5}
         environment="city"
         shadows={{
-          type: "accumulative",
-          color,
-          colorBlend: 2,
-          opacity: 2,
           normalBias: 0.09,
         }}
-        adjustCamera={0.9}
+        adjustCamera={1.5}
       >
-        <Room />
-        <Pop rotation-y={44.8} position={[2.5, 0.54, 2.5]} scale={0.6} />
+        <PresentationControls
+          global
+          azimuth={[-0.6, 0.6]}
+          polar={[0, 0]}
+          snap={{ mass: 3, tension: 200 }}
+        >
+          <Room />
+          <Pop rotation-y={44.8} position={[2.5, 0.54, 2.5]} scale={0.6} />
+        </PresentationControls>
       </Stage>
     </>
   );
