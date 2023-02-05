41382ade47517c645c28d20a5d7b015e22305c48
Mateusz Melaniuk <mateusz.melaniuk98@gmail.com>
2023-02-05 17:10:57 +0100

    Initializing Stage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { OrbitControls } from "@react-three/drei";
+import { Stage, OrbitControls } from "@react-three/drei";
+import Pop from "./Pop";
+import Room from "./Room";
 
 export default function App() {
   return (
     <>
       <OrbitControls makeDefault />
 
-      <mesh scale={1.5}>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <Stage intensity={0.5} environment="city" adjustCamera={0.9}>
+        <Room />
+        <Pop rotation-y={44.8} position={[2.5, 0.54, 2.5]} scale={0.6} />
+      </Stage>
     </>
   );
 }
